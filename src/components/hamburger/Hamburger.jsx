411d5f72fd26d658ef0cd86789dbbea7e16535de
and-gurin <and.gurin@gmail.com>
2023-04-24 16:24:26 +0200

    feat: add keys to map

--- src/components/hamburger/Hamburger.jsx
+++ src/components/hamburger/Hamburger.jsx
@@ -5,7 +5,7 @@ export function Hamburger() {
 
     return (
         <div className={style.hamburger}>
-            {[1, 2, 3].map((item)=><span className={style.hamburger__line}></span>)}
+            {[1, 2, 3].map((item)=><span key={item} className={style.hamburger__line}></span>)}
         </div>
     );
 }
--- src/components/nav/Navigation.jsx
+++ src/components/nav/Navigation.jsx
@@ -1,24 +1,30 @@
 import React from 'react';
 import style from './Navigation.module.scss'
 
-export function Navigation() {
-    const links = [
-        {title: 'Blog', href: '#'},
-        {title: 'About', href: '#about'},
-        {title: 'Links', href: '#links'},
-        {title: 'Projects', href: '#projects'},
-    ]
+const links = [
+    {id: 1, title: 'Blog', href: '#'},
+    {id: 2, title: 'About', href: '#about'},
+    {id: 3, title: 'Links', href: '#links'},
+    {id: 4, title: 'Projects', href: '#projects'},
+]
 
+function NavItem() {
     return (
-
-        <ul className={style.navigation}>
-            {links.map(m =>
-                <li className={style.navigation__item}>
-                    <a className={style.navigation__link} href={m.href}>
-                        {m.title}
+        links.map(item =>
+                <li key={item.id} className={style.navigation__item}>
+                    <a className={style.navigation__link} href={item.href}>
+                        {item.title}
                     </a>
                 </li>
-            )}
+            )
+    )
+
+}
+
+export function Navigation() {
+    return (
+        <ul className={style.navigation}>
+            <NavItem/>
         </ul>
     );
 }
