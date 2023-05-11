28152b084e29316b739be77f5e45d2c8a1895650
and-gurin <and.gurin@gmail.com>
2023-05-11 15:55:29 +0200

    feat: add dangerouslySetInnerHTML and change css rules

--- src/layouts/post/Post.jsx
+++ src/layouts/post/Post.jsx
@@ -10,15 +10,26 @@ import {useParams} from "react-router-dom";
 
 export function Post() {
     const params = useParams();
+    const postId = params.id;
 
-    const [post, setPost] = useState( {})
+    const [post, setPost] = useState({})
+
 
     useEffect(() => {
-        nordicAPI.getPost(params.id)
+        nordicAPI.getPost(postId)
             .then((res) => {
                 setPost(res.data);
             })
-    }, [])
+    }, [postId])
+
+    const options = {day: 'numeric', month: 'long', year: 'numeric',};
+    const setDatetime = post.created_at
+        ? new Date(Date.parse(post.created_at)).toLocaleDateString('en-US', options)
+        : 'wait';
+
+    const setArticleLength = post.content
+        ? Math.round(post.content.replace(/(<([^>]+)>)/ig, '').length / 250)
+        : 'few'
 
     return (
         <section className={style.post}>
@@ -29,11 +40,11 @@ export function Post() {
                              height="56"/>
                         <div>
                             <p className={style.author__name_header}>
-                                Mika Matikainen
+                                Andrei Hurynovich
                             </p>
                             <p className={style.author__description_header}>
-                                <time dateTime="2020-04-15">Apr 15, 2020</time>
-                                · 4 min read
+                                <time>{setDatetime + ' '}</time>
+                                · {' ' + setArticleLength + ' '} min read
                             </p>
                         </div>
                     </div>
@@ -42,17 +53,10 @@ export function Post() {
                     <SocialTableHeader/>
                 </div>
             </div>
-            <p className={style.post__body}>
-                This lovely web is full of everything which is created I don't know what in mind, considering that
-                sometimes totally bonkers stuff might be highly relevant if it has the right kind of thinking behind it.
-            </p>
-            <h4 className={style.post__title}>
-                {post.title}
-            </h4>
-            <p className={style.post__body}>
-                This lovely web is full of everything which is created I don't know what in mind, considering that
-                sometimes totally bonkers stuff might be highly relevant if it has the right kind of thinking behind it.
-            </p>
+            <div
+                className={style.post__body}
+                dangerouslySetInnerHTML={{ __html: post.content }}
+            />
             <figure className={style.post__figure}>
                 <picture>
                     <source media="(max-width: 859px)" srcSet={posImageSmall}/>
@@ -64,14 +68,6 @@ export function Post() {
                     line-height.
                 </figcaption>
             </figure>
-            <p className={style.post__body}>
-                This lovely web is full of everything which is created I don't know what in mind, considering that
-                sometimes totally bonkers stuff might be highly relevant if it has the right kind of thinking behind it.
-                <br/>
-                <br/>
-                Luckily, in the middle of all that, there are some pockets of content that offer delightfully
-                valuable contrast to cursory wisdom in some of the established channels.
-            </p>
             <div className={style.footerTableWrapper}>
                 <SocialTableFooter/>
             </div>
@@ -82,12 +78,12 @@ export function Post() {
             </div>
             <div className={style.author__wrapper_footer}>
                 <div className="author">
-                        <img className={style.author__img_footer} src={author} alt="author" width="56"
-                             height="56"/>
-                        <span className={style.author__name_footer}>
+                    <img className={style.author__img_footer} src={author} alt="author" width="56"
+                         height="56"/>
+                    <span className={style.author__name_footer}>
                         Mika Matikainen
                     </span>
-                        <span className={style.author__description_footer}>
+                    <span className={style.author__description_footer}>
                         is a Design Founder & Advisor, Berlin School of Creative Leadership Executive MBA participant, Zippie advisor, Wolt co-founder, and Nordic Rose stakeholder.
                     </span>
 
